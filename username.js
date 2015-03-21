f59a18d1a24bac7f5d16507c54429c17652f247e
albrofseer1 <albrofseer1@grr.la>
2015-03-21 08:28:03 +0200

    Update username.js

--- username.js
+++ username.js
@@ -1,37 +1,37 @@
-var count = 2;
-function validate() {
-    var un = document.myform.username.value;
-    var pw = document.myform.pword.value;
-    var valid = true;
-    var username = ["1", "ali", "yousif", "thaer", "ahmedhassen", "rekaut", "ahmedmagdy", "ahmedwael"];
-    var password = ["1","test1","test2","test3","test4","test5","test6","test7"];
-    for (var i=0; i <username.length; i++) {
-        if ((un == username[i]) && (pw == password[i])) {
-        valid = false;
-        break;
-        }
-    }
-    if (valid) {
-        alert ("لم يتم التعرف علي المستخدم ؟                                                    سيتم توجيهك الان الي حسابي");
-        window.location = "https://www.facebook.com/profile.php?id=100004016916958";
-        return true;
-    }
-    var t = "";
-    if (count == 1) {t = " try"}
-    if (count >= 1) {
-        alert ("تم التعرف علي المستخدم!");
-        document.myform.username.value = "";
-        document.myform.pword.value = "";
-        setTimeout("document.myform.username.focus()", 25);
-        setTimeout("document.myform.username.select()", 25);
-        count --;
-    }
-    else {
-        alert ("Still incorrect! You have no more tries left!");
-        document.myform.username.value = "No more tries allowed!";
-        document.myform.pword.value = "";
-        document.myform.username.disabled = true;
-        document.myform.pword.disabled = true;
-        return false;
-    }
+function LogIn(){
+loggedin=false;
+username="";
+password="";
+username=prompt("User name:                                      اسم المستخدم","");
+username=username.toLowerCase();
+password=prompt("Password:                                      كلمة السر","");
+password=password.toLowerCase();
+if (username=="ali" && password=="tst") { 
+loggedin=true;
+}
+if (username=="yousif" && password=="tst") {
+loggedin=true;
+}
+if (username=="thaer" && password=="tst") {
+loggedin=true;
+}
+if (username=="michael" && password=="tst") { 
+loggedin=true;
+}
+if (username=="ahmedhassen" && password=="tst") {
+loggedin=true;
+}
+if (username=="rekaut" && password=="tst") {
+loggedin=true;
+}
+if (username=="ahmedmagdy" && password=="tst") {
+loggedin=true;
+}
+if (username=="ahmedwael" && password=="tst") {
+loggedin=true;
+}
+if (loggedin==false) {
+alert("لم يتم التعرف علي اسم المستخدم سيتم تحويلة الان الي حسابي");
+window.location="home-page.html";
+}
 }
