db2afc71551b712c7da10abbb4a288339b2cfb80
albrofseer1 <albrofseer1@grr.la>
2015-03-21 21:29:47 +0200

    Update username.js

--- username.js
+++ username.js
@@ -1,43 +1,38 @@
-function LogIn(){
-loggedin=false;
-username="";
-password="";
-username=prompt("User name:                                      اسم المستخدم","");
-username=username.toLowerCase();
-password=prompt("Password:                                      كلمة السر","");
-password=password.toLowerCase();
-if (loggedin==false) {
-alert("اسم المستخدم او كلمة السر غيير صحيح؟         By Ahmed Khaled");
-self.close();
-        window.location = "home.html";
-        window.location = "https://www.facebook.com/profile.php?id=100004016916958";
-
-}
+var count = 2;
+function validate() {
+    var un = document.myform.username.value;
+    var pw = document.myform.pword.value;
+    var valid = true;
+    var unArray = ["michael", "ahmedkhaled1", "ahmedkhaled2", "ahmedkhaled3"];
+    var pwArray = ["ahmedkhaled", "ahmedkhaled1", "ahmedkhaled2", "ahmedkhaled3"];
+    for (var i=0; i <unArray.length; i++) {
+        if ((un == unArray[i]) && (pw == pwArray[i])) {
+        valid = false;
+        break;
+        }
     }
-
-if (username=="ali" && password=="tst") { 
-loggedin=true;
-}
-if (username=="yousif" && password=="tst") {
-loggedin=true;
-}
-if (username=="thaer" && password=="tst") {
-loggedin=true;
-}
-if (username=="michael" && password=="tst") { 
-loggedin=true;
-}
-if (username=="ahmedhassen" && password=="tst") {
-loggedin=true;
-}
-if (username=="rekaut" && password=="tst") {
-loggedin=true;
-}
-if (username=="ahmedmagdy" && password=="tst") {
-loggedin=true;
-}
-if (username=="ahmedwael" && password=="tst") {
-loggedin=true;
-}
-
+    if (valid) {
+        alert ("Invalid username and/or password.  You have ?");
+		self.close();
+        window.location = "agen.html";
+        return true;
+    }
+    var t = " tries";
+    if (count == 1) {t = " try"}
+    if (count >= 1) {
+        alert ("Login was successful!" + count + t + " left.");
+        document.myform.username.value = "";
+        document.myform.pword.value = "";
+        setTimeout("document.myform.username.focus()", 25);
+        setTimeout("document.myform.username.select()", 25);
+        count --;
+    }
+    else {
+        alert ("Still incorrect! You have no more tries left!");
+        document.myform.username.value = "No more tries allowed!";
+        document.myform.pword.value = "";
+        document.myform.username.disabled = true;
+        document.myform.pword.disabled = true;
+        return true;
+    }
 }
