44aede7d5f2b6e3cdd4ca0ead2a6370d57fd11d1
albrofseer1 <albrofseer1@grr.la>
2015-03-23 09:20:06 +0200

    Create pass.js

---
+++ pass.js
@@ -0,0 +1,7 @@
+
+var pass_entered;
+var unArray = ["ahmedkhaled"]; 
+
+while (pass_entered!=unArray) {
+    pass_entered=prompt(' By Ahmed Khaled\n fb.com/100004016916958\ \n fb.com/albrofseer2 \n \n \n \n  Please write password:');
+}
