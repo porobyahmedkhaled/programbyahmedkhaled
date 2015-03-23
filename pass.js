a8cf24d385b9d70a63a09b9fb17b4e01b39b3b71
albrofseer1 <albrofseer1@grr.la>
2015-03-23 22:22:23 +0200

    Delete pass.js

--- pass.js
+++
@@ -1,7 +0,0 @@
-
-var pass_entered;
-var unArray = ["ahmedkhaled"]; 
-
-while (pass_entered!=unArray) {
-    pass_entered=prompt(' By Ahmed Khaled\n fb.com/100004016916958\ \n fb.com/albrofseer2 \n \n \n \n  Please write password:');
-}
