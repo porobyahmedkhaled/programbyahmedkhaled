165f2f3dd1ec46445c7402064a19b1e66545bb09
albrofseer1 <albrofseer1@grr.la>
2015-05-05 18:48:30 +0300

    Delete pass.js

--- pass.js
+++
@@ -1,6 +0,0 @@
-var pass_entered;
-var password="ahmedkhaled_top";
-
-while (pass_entered!=password) {
-    pass_entered=prompt("Ahmed Khaled only one\nfb.com/100004016916958\nfb.com/albrofseer2\n\nPassword:               :كلمة السر الخاصة بالبرنامج","");
-}
